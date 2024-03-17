15ea0dc4a98f66e2acf6bd19bd733308420e116c
hriaz17 <harisriaz1700@gmail.com>
2024-03-17 16:43:47 -0700

    Initial commit

---
+++ csc583/query_engine.py
@@ -0,0 +1,54 @@
+from csc583.document import Document
+
+class QueryEngine:
+
+    def __init__(self,input_file):
+        # add your code here
+        pass
+
+    def read_txt_file(self,input_file):
+        # add your code here
+        docs=None
+        return docs
+
+    def q1_1(self, query):
+        # This is just sample code. Add your actual code here.
+        # The Document class we provided is just a dummy wrapper over Lucene document.
+        # the document you use must be the Lucene document i.e doc = document.Document()
+        ans=[]
+        # first argument must be of type document.Document()
+        return ans
+
+    def q1_2_a(self, query):
+        # This is just sample code. add your actual code here.
+        # The Document class we provided is just a dummy wrapper over Lucene document.
+        # the document you use must be the Lucene document i.e doc = document.Document()
+        ans=[]
+        # first argument must be of type document.Document()
+        return ans
+
+
+    def q1_2_b(self, query):
+        # This is just sample code. add your actual code here.
+        # The Document class we provided is just a dummy wrapper over Lucene document.
+        # the document you use must be the Lucene document i.e doc = document.Document()
+        ans =[]
+        # first argument must be of type document.Document()
+        return ans
+
+    def q1_2_c(self, query):
+        # This is just sample code. add your actual code here.
+        # The Document class we provided is just a dummy wrapper over Lucene document.
+        # the document you use must be the Lucene document i.e doc = document.Document()
+        ans =[]
+        # first argument must be of type document.Document()
+        return ans
+
+    def q1_3(self, query):
+        # This is just sample code. add your actual code here.
+        # The Document class we provided is just a dummy wrapper over Lucene document.
+        # the document you use must be the Lucene document i.e doc = document.Document()
+        ans =[]
+        # first argument must be of type document.Document()
+        return ans
+
